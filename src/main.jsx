312a0349479601c93f2fb88113b41494afd80185
Mathias Wagner <germannewsmaker@gmail.com>
2024-05-26 00:17:21 +0200

    Created the main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import "@/assets/styles/main.sass";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        <h2>Hello, world!</h2>
+    </StrictMode>
+);
