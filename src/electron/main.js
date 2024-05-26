25582da5b4d9289bb737a5d3a60bf61e83033466
Mathias Wagner <germannewsmaker@gmail.com>
2024-05-26 02:41:34 +0200

    Implemented serial connections in the main.js

--- src/electron/main.js
+++ src/electron/main.js
@@ -1,5 +1,16 @@
-import {app, BrowserWindow} from "electron";
-import path from "node:path";
+const path = require("node:path");
+const { app, BrowserWindow } = require("electron");
+const {SerialPort} = require("serialport");
+const {ReadlineParser} = require("@serialport/parser-readline");
+
+const port = new SerialPort({ path: "/dev/ttyACM0", baudRate: 9600 });
+port.on("open", () => {
+    console.log("Coin acceptor connected");
+});
+
+const parser = port.pipe(new ReadlineParser());
+parser.on('data', console.log);
+
 
 const createWindow = () => {
     const win = new BrowserWindow({
