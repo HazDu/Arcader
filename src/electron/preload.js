7f9f4ece83cbe34981cafa8e064eb8f8c397287b
Mathias Wagner <germannewsmaker@gmail.com>
2024-05-26 13:58:22 +0200

    Updated the preload.js

--- src/electron/preload.js
+++ src/electron/preload.js
@@ -1,1 +1,10 @@
-console.log("run preload.js");
+const {contextBridge,ipcRenderer} = require('electron');
+
+contextBridge.exposeInMainWorld('electron', {
+    receive: (channel, func) => {
+        ipcRenderer.on(channel, func);
+    },
+    send: (channel, data) => {
+        ipcRenderer.send(channel, data);
+    },
+});
