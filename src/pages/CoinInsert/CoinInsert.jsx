22a7d33dcab4de2fb679939c3ca86fb076cc7083
Mathias Wagner <germannewsmaker@gmail.com>
2024-05-27 21:32:20 +0200

    Implemented coin detection in the CoinInsert.jsx

--- src/pages/CoinInsert/CoinInsert.jsx
+++ src/pages/CoinInsert/CoinInsert.jsx
@@ -1,6 +1,23 @@
 import "./styles.sass";
+import {useNavigate} from "react-router-dom";
+import {useEffect} from "react";
 
 export const CoinInsert = () => {
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            window.electron.receive("coin-detected", () => {
+                navigate("/home");
+            });
+        }, 1000);
+
+        return () => {
+            clearTimeout(timeout);
+        }
+    }, []);
+
     return (
         <div className="insert-page">
             <h1>INSERT COIN</h1>
