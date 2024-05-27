0d169218b570bafa3f75d0d83d810c8dbbce4bc6
Mathias Wagner <germannewsmaker@gmail.com>
2024-05-27 21:13:28 +0200

    Updated the main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,31 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import "@/assets/styles/main.sass";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Loading from "@/pages/Loading";
+import CoinInsert from "@/pages/CoinInsert";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Loading />
+    },
+    {
+        path: "/coin",
+        element: <CoinInsert />
+    },
+    {
+        path: "/error",
+        element: <div>ERROR</div>
+    },
+    {
+        path: "*",
+        element: <Loading />
+    }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <h1>LOADING...</h1>
+        <RouterProvider router={router} />
     </StrictMode>
 );
