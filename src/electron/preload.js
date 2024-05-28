5e78da4ed1ee37cb44ba75547c662974ad177aa3
Mathias Wagner <germannewsmaker@gmail.com>
2024-05-28 18:10:02 +0200

    Created the endReceive functions in the preload.js

--- src/electron/preload.js
+++ src/electron/preload.js
@@ -4,6 +4,12 @@ contextBridge.exposeInMainWorld('electron', {
     receive: (channel, func) => {
         ipcRenderer.on(channel, func);
     },
+    endReceive: (channel, func) => {
+        ipcRenderer.off(channel, func);
+    },
+    endReceiveAll: (channel) => {
+        ipcRenderer.removeAllListeners(channel);
+    },
     send: (channel, data) => {
         ipcRenderer.send(channel, data);
     },
