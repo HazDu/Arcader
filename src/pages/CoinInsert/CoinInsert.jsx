b4a4ecd0c444bc13105360d584aeb86129b022d6
Mathias Wagner <germannewsmaker@gmail.com>
2024-05-29 02:57:42 +0200

    Updated the CoinInsert.jsx

--- src/pages/CoinInsert/CoinInsert.jsx
+++ src/pages/CoinInsert/CoinInsert.jsx
@@ -1,6 +1,8 @@
 import "./styles.sass";
 import {useNavigate} from "react-router-dom";
 import {useEffect} from "react";
+import AcceptorImage from "./assets/acceptor.png";
+import CoinImage from "./assets/coin.png";
 
 export const CoinInsert = () => {
 
@@ -21,6 +23,11 @@ export const CoinInsert = () => {
     return (
         <div className="insert-page">
             <h1>INSERT COIN</h1>
+            <div className="coin-animation">
+                <img src={AcceptorImage} alt="Coin Acceptor" className="acceptor"/>
+                <img src={CoinImage} alt="Coin" className="coin"/>
+            </div>
+            <h2 className="sub-text">Insert Coin to enter Game Library and choose a Game to play!</h2>
         </div>
     )
 }
