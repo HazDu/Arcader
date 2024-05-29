f2b17dc51254479be3d48759744c3b91674f7f3f
Mathias Wagner <germannewsmaker@gmail.com>
2024-05-29 03:14:07 +0200

    Updated the main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,27 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import "@/assets/styles/main.sass";
+import "@/common/assets/styles/main.sass";
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Loading from "@/pages/Loading";
 import CoinInsert from "@/pages/CoinInsert";
 import Home from "@/pages/Home";
+import App from "@/common/layouts/App";
+import GameSelect from "@/pages/GameSelect";
+import Search from "@/pages/Search";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <Loading />
-    },
-    {
-        path: "/coin",
-        element: <CoinInsert />
-    },
-    {
-        path: "/error",
-        element: <div>ERROR</div>
-    },
-    {
-        path: "/home",
-        element: <Home />
-    },
-    {
-        path: "*",
-        element: <Loading />
+        element: <App />,
+        children: [
+            {path: "/", element: <Loading />},
+            {path: "/coin", element: <CoinInsert />},
+            {path: "/error", element: <h2>ERROR: Coin detector not connected</h2>},
+            {path: "/home", element: <Home />},
+            {path: "/game-select", element: <GameSelect />},
+            {path: "/search", element: <Search />},
+            {path: "*", element: <Loading />}
+        ]
     }
 ]);
 
