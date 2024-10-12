5ad8f3416446eed7224601776d1b43304c85d7a2
Mathias Wagner <germannewsmaker@gmail.com>
2024-10-12 18:31:29 +0200

    Integrate custom port support into the api index.js

--- src/main/api/index.js
+++ src/main/api/index.js
@@ -1,13 +1,25 @@
 import express from "express";
+import imageRouter from "./routes/cache";
 
 const app = express();
+const SERVER_PORT = process.env.ADMIN_UI_PORT || 5328;
+
+const ignoreCORS = (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+}
+
+app.use(express.json());
+app.use(ignoreCORS);
+
+app.use("/image", imageRouter);
 
 app.get("/", (req, res) => {
     res.send("admin ui");
 });
 
 export const startServer = () => {
-    app.listen(5328, () => {
-        console.log("Server started on http://localhost:5328");
+    app.listen(SERVER_PORT, () => {
+        console.log(`Server started on port ${SERVER_PORT}`);
     });
 }
